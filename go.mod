e1839b45987987f41a426ec385307d56e0136d5f
Victor LABORIE <vlaborie@posteo.net>
2020-04-18 17:03:12 +0200

    Use spf13/cobra for CLI interactions

--- go.mod
+++ go.mod
@@ -2,5 +2,6 @@ module github.com/vlaborie/remarkable-sync
 
 require (
 	github.com/bmaupin/go-epub v0.5.3
+	github.com/spf13/cobra v1.0.0
 	miniflux.app v0.0.0-20200328215126-9698c5e40ada
 )
--- main.go
+++ main.go
@@ -1,40 +1,9 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
-
-	"github.com/vlaborie/remarkable-sync/remarkable"
-
-	"github.com/bmaupin/go-epub"
+	"github.com/vlaborie/remarkable-sync/cmd"
 )
 
 func main() {
-	Remarkable := remarkable.New("/home/root/.local/share/remarkable/xochitl/")
-
-	wallabagConfig, err := os.Open("/etc/remarkable-sync/wallabag.json")
-	if err == nil {
-		Remarkable.Wallabag(wallabagConfig)
-	}
-
-	minifluxConfig, err := os.Open("/etc/remarkable-sync/miniflux.json")
-	if err == nil {
-		Remarkable.Miniflux(minifluxConfig)
-	}
-
-	for _, RemarkableItem := range Remarkable.Items {
-		if RemarkableItem.ContentType == "html" {
-			RemarkableItem.ContentType = "epub"
-			e := epub.NewEpub(RemarkableItem.VisibleName)
-			e.AddSection(string(RemarkableItem.Content), "Section 1", "", "")
-			e.Write(Remarkable.Dir + RemarkableItem.Id + "." + RemarkableItem.ContentType)
-			fmt.Println("EPUB of " + RemarkableItem.Id + " writed")
-		}
-
-		j, _ := json.Marshal(RemarkableItem)
-		_ = ioutil.WriteFile(Remarkable.Dir+RemarkableItem.Id+".metadata", j, 0644)
-		fmt.Println("Metadata of " + RemarkableItem.Id + " updated")
-	}
+	cmd.Execute()
 }
